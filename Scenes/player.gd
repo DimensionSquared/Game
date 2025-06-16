5756f6588773db952a8d309971bfdbb07d347fc7
DimensionSquared <zachary.tinsel@gmail.com>
2025-06-16 12:38:32 +1200

    Added part of player movment script and fixed player res

---
+++ Scenes/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	var direction = Input.get_axis("Move_left","Move_right")
+	velocity.x = direction.x * SPEED
+
+	move_and_slide()
